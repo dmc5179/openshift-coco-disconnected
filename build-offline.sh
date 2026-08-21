e5693c6ab65d3e8985739bd4a3a60a6bd7ad7574
Dan Clark <danclark@redhat.com>
2026-08-21 19:22:18 -0400

    Remove old build notes

--- build-offline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Build pcr collection container image
-podman build --squash -t quay.io/danclark/pcr-collector:latest -f Containerfile-pcr-collector .
-
-podman push --authfile=/home/danclark/quay-pull-secret.json quay.io/danclark/pcr-collector:latest
